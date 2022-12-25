c8aaa69e6857464d3af0686083e9a8ae92e7253c
yel-mrab <yahya181elmrabti@gmail.com>
2022-12-25 22:26:15 +0100

    feat: equal function was added

---
+++ inc/equal/equal.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   equal.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/25 21:55:11 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/12/25 22:20:11 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EQUAL_HPP
+#define EQUAL_HPP
+
+namespace ft{
+	
+	template <class InputIterator1, class InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2){
+		while (first1 != last1){
+			if (*first1 != *first2)
+				return (false);
+			first1++;
+			first2++;
+		}
+		return (true);
+	}
+
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred){
+		while (first1 != last1){
+			if (!pred(*first1, *first2))
+				return (false);
+			first1++;
+			first2++;
+		}
+		return (true);
+	}
+}
+
+#endif
