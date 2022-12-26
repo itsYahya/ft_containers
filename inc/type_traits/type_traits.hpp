5bfd36732d609e8a0018ff52def54481f68da555
yel-mrab <yahya181elmrabti@gmail.com>
2022-12-26 16:25:42 +0100

    feat: enable_if implementation

--- inc/type_traits/type_traits.hpp
+++ inc/type_traits/type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 14:57:32 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/12/26 16:14:26 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2022/12/26 16:23:39 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,100 +15,92 @@
 
 namespace ft{	
 
+	template <bool Cond, class T = void>
+	struct enable_if {};
+	
 	template <class T>
-	struct is_integral
-	{
+	struct enable_if <true, T>{
+		typedef T	type;
+	};
+	
+	template <class T>
+	struct is_integral{
 		typedef	T			value_type;
 		static const bool	value = false;
 	};
 	
 	template <>
-	struct is_integral<bool>
-	{
+	struct is_integral<bool>{
 		static const bool	value = true;
 	};
 	
 	template <>
-	struct is_integral<char>
-	{
+	struct is_integral<char>{
 		static const bool	value = true;
 	};
 	
 	template <>
-	struct is_integral<char16_t>
-	{
+	struct is_integral<char16_t>{
 		static const bool	value = true;
 	};
 	
 	template <>
-	struct is_integral<char32_t>
-	{
+	struct is_integral<char32_t>{
 		static const bool	value = true;
 	};
 	
 	template <>
-	struct is_integral<wchar_t>
-	{
+	struct is_integral<wchar_t>{
 		static const bool	value = true;
 	};
 	
 	template <>
-	struct is_integral<signed char>
-	{
+	struct is_integral<signed char>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<short int>
-	{
+	struct is_integral<short int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<int>
-	{
+	struct is_integral<int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<long int>
-	{
+	struct is_integral<long int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<long long int>
-	{
+	struct is_integral<long long int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<unsigned char>
-	{
+	struct is_integral<unsigned char>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<unsigned short int>
-	{
+	struct is_integral<unsigned short int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<unsigned int>
-	{
+	struct is_integral<unsigned int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<unsigned long int>
-	{
+	struct is_integral<unsigned long int>{
 		static const bool	value = true;
 	};
 
 	template <>
-	struct is_integral<unsigned long long int>
-	{
+	struct is_integral<unsigned long long int>{
 		static const bool	value = true;
 	};
 }
