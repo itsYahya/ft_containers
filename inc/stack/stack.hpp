d81251a1ced1400390fe5b446751cbf811069e61
yel-mrab <yahya181elmrabti@gmail.com>
2022-12-27 19:09:43 +0100

    feat: relational opearators were added to the stack class

--- inc/stack/stack.hpp
+++ inc/stack/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 16:35:30 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/12/27 17:13:54 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2022/12/27 19:09:15 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ namespace ft{
 	template <class T, class Container = ft::vector<T> >
 	class stack
 	{
-		private:
+		protected:
 			Container	c;
 			
 		public:
-			typedef	T			value_type;
-			typedef Container	container_type;
-			typedef size_t		size_type;
+			typedef	T									value_type;
+			typedef Container							container_type;
+			typedef typename container_type::size_type	size_type;
 			
 			explicit stack(const container_type &c = container_type()){
 				this->c = c;
@@ -58,8 +58,50 @@ namespace ft{
 			void	pop(){
 				c.pop_back();
 			}
+			template <class TP, class C>
+			friend bool	operator==(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
+			template <class TP, class C>
+			friend bool	operator!=(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
+			template <class TP, class C>
+			friend bool	operator<(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
+			template <class TP, class C>
+			friend bool	operator<=(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
+			template <class TP, class C>
+			friend bool	operator>(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
+			template <class TP, class C>
+			friend bool	operator>=(const ft::stack<TP, C> &s1, const ft::stack<TP, C> &s2);
 	};
 	
+	template <class T, class Container>
+	bool	operator==(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c == s2.c);
+	}
+
+	template <class T, class Container>
+	bool	operator!=(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c != s2.c);
+	}
+
+	template <class T, class Container>
+	bool	operator<(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c < s2.c);
+	}
+
+	template <class T, class Container>
+	bool	operator<=(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c <= s2.c);
+	}
+
+	template <class T, class Container>
+	bool	operator>(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c > s2.c);
+	}
+
+	template <class T, class Container>
+	bool	operator>=(const ft::stack<T, Container> &s1, const ft::stack<T, Container> &s2){
+		return (s1.c >= s2.c);
+	}
+
 }
 
 #endif
