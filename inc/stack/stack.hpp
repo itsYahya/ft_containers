11c199f9a821b69cc05fb3a85140483068234ec9
yel-mrab <yahya181elmrabti@gmail.com>
2022-12-27 17:09:16 +0100

    feat: initial imlementation of the stack class

---
+++ inc/stack/stack.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 16:35:30 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/12/27 17:08:49 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+namespace ft{
+	
+	template <class T, class Container = ft::vector<T> >
+	class stack
+	{
+		private:
+			Container	c;
+			
+		public:
+			typedef	T			value_type;
+			typedef Container	container_type;
+			typedef size_t		size_type;
+			
+			
+	};
+	
+}
+
+#endif
