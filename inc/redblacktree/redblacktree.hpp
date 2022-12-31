47f0f0fc70fad91f09e332a22f56d91d48860fd9
yel-mrab <yahya181elmrabti@gmail.com>
2022-12-31 18:50:15 +0100

    feat: intial implementation of the node class

---
+++ inc/redblacktree/redblacktree.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redblacktree.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 19:43:52 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/12/31 18:50:33 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REDBLACKTREE_HPP
+#define REDBLACKTREE_HPP
+
+namespace ft{
+	template <class T>
+	class Node{
+		public:
+			typedef T				value_type;
+			typedef Node<T>*		pointer;
+			typedef const pointer	const_pointer;
+
+			value_type	data;
+			bool		color;
+			pointer		right;
+			pointer		left;
+			pointer		parent;
+		
+	};
+}
+
+#endif
