1c18f7617f0aa1800dae0ef48b858b6e90959040
yel-mrab <yahya181elmrabti@gmail.com>
2023-01-08 17:04:57 +0100

    fix: value_type in the bidirectional iterator

--- inc/iterators/bidirection_iterator.hpp
+++ inc/iterators/bidirection_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 23:58:42 by yel-mrab          #+#    #+#             */
-/*   Updated: 2023/01/08 03:47:21 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2023/01/08 17:03:30 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@ namespace ft{
 	template <class T>
 	class bidirection_iterator {
 		public:
-			typedef T												value_type;
-			typedef typename iterator_traits<T*>::difference_type	difference_type;
-			typedef typename iterator_traits<T*>::pointer			pointer;
-			typedef typename iterator_traits<T*>::reference			reference;
-			typedef std::bidirectional_iterator_tag					iterator_category;
-			typedef bidirection_iterator<T>							iterator_type;
+			typedef T														node_type;
+			typedef typename T::value_type									value_type;
+			typedef typename iterator_traits<value_type*>::difference_type	difference_type;
+			typedef typename iterator_traits<value_type*>::pointer			pointer;
+			typedef typename iterator_traits<value_type*>::reference		reference;
+			typedef std::bidirectional_iterator_tag							iterator_category;
+			typedef bidirection_iterator<T>									iterator_type;
 
 		private:
-			pointer	ptr;
+			node_type*	ptr;
 		
 		public:
 			bidirection_iterator() : ptr(nullptr) {}
 			
-			bidirection_iterator(pointer p) : ptr(p) {}
+			bidirection_iterator(node_type* p) : ptr(p) {}
 
 			bidirection_iterator(const iterator_type &iter) : ptr(iter.ptr) {}
 			
@@ -53,32 +54,32 @@ namespace ft{
 			}
 
 			reference	operator*() const{
-				return (*ptr);
+				return (ptr->data);
 			}
 
 			pointer		operator->() const{
-				return (ptr);
+				return (&ptr->data);
 			}
 
 			iterator_type	&operator++(){
-				ptr = value_type::successor(this->ptr);
+				ptr = node_type::successor(this->ptr);
 				return (*this);
 			}
 			
 			iterator_type	operator++(int){
 				iterator_type iter(*this);
-				ptr = value_type::successor(this->ptr);
+				ptr = node_type::successor(this->ptr);
 				return (iter);
 			}
 			
 			iterator_type	&operator--(){
-				ptr = value_type::predecessor(this->ptr);
+				ptr = node_type::predecessor(this->ptr);
 				return (*this);
 			}
 			
 			iterator_type	operator--(int){
 				iterator_type iter(*this);
-				ptr = value_type::predecessor(this->ptr);
+				ptr = node_type::predecessor(this->ptr);
 				return (iter);
 			}
 	};
