d0eaa383e2771872c30aeff0165f6ebcebbf1e58
yel-mrab <yahya181elmrabti@gmail.com>
2023-01-24 13:47:19 +0100

    feat: adding some new test for the vector class

--- src/vector_tests.cpp
+++ src/vector_tests.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   vector_tests.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:37:30 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/12/26 19:58:15 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:44:07 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,110 +14,80 @@
 #include "../inc/vector/vector.hpp"
 #include <vector>
 
+void	test(const ft::vector<int> &vector)
+{
+	std::cout << "++++++++ const iterators test ++++++++++++" << std::endl;
+	
+	ft::vector<int>::const_iterator iter = vector.begin();
+	//*iter = 44; // you can't actually do this cause this iterator is pointing on a const type of date
+	for (; iter != vector.end(); iter++)
+		std::cout << *iter << std::endl;
+	std::cout << "+++++++++ reverse +++++++++++" << std::endl;
+	ft::vector<int>::const_reverse_iterator riter = vector.rbegin();
+	for (; riter != vector.rend(); riter++)
+		std::cout << *riter << std::endl;
+}
+
+void	printv(const ft::vector<int> &vector){
+	ft::vector<int>::const_iterator iter = vector.begin();
+	for (; iter != vector.end(); iter++)
+		std::cout << *iter << std::endl;
+}
+
 int main(){
+	typedef ft::vector<int>::iterator iterator;
+	typedef ft::vector<int>::reverse_iterator reverse_iterator;
 	ft::vector<int> vector, v;
 	
-	vector.push_back(991);
-	vector.push_back(992);
-	vector.push_back(993);
-	vector.push_back(994);
-	vector.push_back(995);
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
 
-	v.push_back(909);
-	v.push_back(929);
-	v.push_back(949);
-	v.push_back(989);
+	v.push_back(9091);
+	v.push_back(9292);
+	v.push_back(9493);
+	v.push_back(9894);
 	
-	// vector = v;
-	// std::cout << v.size() << std::endl;
-	// std::cout << vector.size() << std::endl;
+	std::cout << "+++++++++++++++ iterators test ++++++++++" << std::endl;
+	iterator iter = v.begin(), it = v.begin();
+	for (; iter != v.end(); iter++)
+		std::cout << *iter << std::endl;
+		
+	std::cout << "++++++++++ reverse +++++++++++++" << std::endl;
+	reverse_iterator riter = v.rbegin();
+	for (; riter != v.rend(); riter++)
+		std::cout << *riter << std::endl;
+	*it = 69; // here you can assign and change the data that this iterator is pointing on
+	test(v);
 	
-	// ft::vector<int>::iterator iter = vector.begin();
-	// for (int i = 1; iter != vector.end(); iter++, i++){
-	// 	std::cout << "element number " << i << " : " << *iter << std::endl;
-	// }
+	ft::vector<int> v1(v);
+	printv(v1);
+	v1.swap(vector);
+	std::cout << "++++++++++ after swap +++++++++" << std::endl;
+	printv(v1);
+	
+	iterator from = v1.begin(), until = v1.end() - 2;
+	std::cout << until - from << std::endl;
+	ft::vector<int> v2(from, until);
+	std::cout << "the vector capacity is : " << v2.capacity() << std::endl;
 
-	// ft::vector<int>::iterator iter = vv.begin();
-	// std::cout << vv.end() - iter << std::endl; 
-	
-	// vector.swap(v);
-	// ft::vector<int>::iterator iter = v.begin();
-	// for (; iter != v.end(); iter++){
-	// 	std::cout << *iter << std::endl;
-	// }
-	// std::vector<int>::iterator iter = vector.erase(vector.begin(), vector.end() - 2);
-	// iter = vector.begin();
-	// for (; iter != vector.end(); iter++){
-	// 	std::cout << *iter << std::endl;
-	// }
-	
-	// // data function test
-	// int	*ptr = vector.data();
-	// *ptr = 88;
-	// std::cout << *ptr << std::endl;
-	
-	// // back and fron functions test
-	// std::cout << "back element : " << vector.back() << std::endl;
-	// std::cout << "front element : " << vector.front() << std::endl;
-	
-	// // // at function test
-	// try{
-	// 	std::cout << vector.at(400) << std::endl;
-	// }catch (std::exception &e){
-	// 	std::cout << e.what() << std::endl;
-	// }
-
-	// // [] operator test
-	// std::cout << vector[4] << std::endl;
-
-	// // reserve function test
-	// std::cout << vector.capacity() << std::endl;
-	// vector.reserve(15);
-	// std::cout << vector.capacity() << std::endl;
-	
-	// // resize function test
-	// std::cout << "+++++++++ before resize ++++++++" << std::endl;
-	// std::cout << "vector size : " << vector.size() << std::endl;
-	// std::cout << "vector capacity : " << vector.capacity() << std::endl;
-	// ft::vector<int>::iterator iter = vector.begin();
-	// for (; iter != vector.end(); iter++){
-	// 	std::cout << *iter << std::endl;
-	// }
-	// vector.resize(10);
-	// std::cout << "+++++++++ after resize ++++++++" << std::endl;
-	// std::cout << "vector size : " << vector.size() << std::endl;
-	// std::cout << "vector capacity : " << vector.capacity() << std::endl;
-	// iter = vector.begin();
-	// for (; iter != vector.end(); iter++){
-	// 	std::cout << *iter << std::endl;
-	// }
-	
-	// // iterators test
-	// std::cout << "+++++++++++ iterator ++++++++++" << std::endl;
-	// ft::vector<int>::iterator iter = vector.begin();
-	// for (; iter != vector.end(); iter++){
-	// 	std::cout << *iter << std::endl;
-	// }
-	// std::cout << "+++++++++++ reverse iterator ++++++++++" << std::endl;
-	// ft::vector<int>::reverse_iterator riter = vector.rbegin();
-	// for (; riter != vector.rend(); riter++){
-	// 	std::cout << *riter << std::endl;
-	// }
-	// std::cout << "+++++++++++ const iterator ++++++++++" << std::endl;
-	// ft::vector<int> const cv(6, 55);
-	// ft::vector<int>::const_iterator citer = cv.begin();
-	// for (; citer != cv.end(); citer++){
-	// 	std::cout << *citer << std::endl;
-	// }
-	
-	// TODO		const_iterator test
-
-	// // some functions test
-	// std::cout << "+++++++++++++++++++++" << std::endl;
-	// std::cout << vector.size() << std::endl;
-	// std::cout << vector.capacity() << std::endl;
-	// std::cout << vector.max_size() << std::endl;
-	// std::cout << vector.empty() << std::endl;
-	
+	v1.resize(10, 1);
+	std::cout << "after resising the vector " << std::endl;
+	printv(v1);
+	v1.resize(4);
+	std::cout << "++++++0" << std::endl;
+	printv(v1);
+	std::cout << v1.capacity() << std::endl;
+	std::cout << v1.size() << std::endl;
+	// system("leaks container");
+	size_t size = v1.max_size();
+	try {
+		v1.reserve(size+10);
+		std::cout << "everything went as expected" << std::endl;
+	}catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
 	return (0);	
 }
